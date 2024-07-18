4f3effbb72b35b25fb03e262ae114fb0515eb3e9
msed-Ebrahimi <me00018@mix.wvu.edu>
2024-07-18 11:39:56 -0400

    first push

---
+++ configs/ms1mv3_r100.py
@@ -0,0 +1,31 @@
+from easydict import EasyDict as edict
+
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
+config = edict()
+config.margin_list = (1.0, 0.5, 0.0)
+config.network = "r100"
+config.resume = False
+config.output = None
+config.embedding_size = 512
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 128
+config.lr = (0.1*config.batch_size*4)/(1024)
+config.verbose = 2000
+config.dali = False
+
+config.rec = "/train_tmp/ms1m-retinaface-t1"
+config.num_classes = 93431
+config.num_image = 5179510
+config.num_epoch = 20
+config.warmup_epoch = 0
+config.val_targets = []
+config.eta_scale = 0.1
+config.eta_t = 0.1
+config.eta_theta = 0.1
+config.ratio = 0.75
